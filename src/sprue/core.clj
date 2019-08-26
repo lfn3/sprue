24a00ba2200a53326cce8b74b4a5ad8da21860c5
Liam Falconer <liam@lfn3.net>
2019-08-26 15:48:04 +0100

    Id classes.

--- src/sprue/core.clj
+++ src/sprue/core.clj
@@ -1,5 +1,5 @@
 (ns sprue.core
-  (:import [com.squareup.kotlinpoet TypeSpec FileSpec KModifier FunSpec FunSpec$Builder TypeSpec$Builder PropertySpec]
+  (:import [com.squareup.kotlinpoet TypeSpec FileSpec KModifier FunSpec FunSpec$Builder TypeSpec$Builder PropertySpec ClassName]
            (java.io StringWriter))
   (:gen-class))
 
@@ -23,34 +23,73 @@
 
 (defn make-data-class [{:keys [^String name fields]}]
   (let [builder (doto (TypeSpec/classBuilder name)
-                (.primaryConstructor (build-ctor fields))
-                (.addModifiers (k-mods KModifier/DATA)))]
+                  (.primaryConstructor (build-ctor fields))
+                  (.addModifiers (k-mods KModifier/DATA)))]
     (->> fields
          (map (partial add-property builder))
          (dorun))
     (.build builder)))
 
-(def sample-config {:template {:package "net.lfn3.sprue"}
-                    :classes  [{:name   "Test"
-                                :fields [{:name "name"
-                                          :type String}]}]})
-
-(defn build [{:keys [template classes] :as config}]
-  (->> classes
-       (map (partial merge template))
-       (map make-data-class)))
-
-(defn str-class [config]
-  (let [fs (FileSpec/builder "net.lfn3.sprue" "Sprue")]
-    (->> config
-         (build)
-         (map #(.addType fs %1))
+(defn make-id-class [{:keys [^String name extends]}]
+  (let [fields [{:name "id"
+                 :type Long/TYPE}]
+        [extend-package extends-name] extends
+        builder (doto (TypeSpec/classBuilder name)
+                  (.primaryConstructor (build-ctor fields))
+                  (.superclass (ClassName. extend-package extends-name (into-array String [])))
+                  (.addSuperclassConstructorParameter "id" (into-array Object [])))]
+    (->> fields
+         (map (partial add-property builder))
          (dorun))
-    (let [sw (StringWriter.)]
-      (.writeTo (.build fs) sw)
-      (str sw))))
+    (.build builder)))
+
+(defmulti make-class :type)
+(defmethod make-class :data [class-config] (make-data-class class-config))
+(defmethod make-class :id [class-config] (make-id-class class-config))
+
+(defn id-for [{:keys [name package] :as class-config}]
+  {:name    (str name "Id")
+   :type    :id
+   :package package})
+
+(defn with-ids [classes]
+  (concat classes (map id-for classes)))
+
+(def type-keys #{:id :data})
+
+(defn merge-all-template [templates]
+  (->> type-keys
+       (map (fn [k] [k (merge (:all templates) (get templates k))]))
+       (into {})))
+
+(defn merge-templates [templates classes]
+  (->> classes
+       (map #(merge %1 (get templates (:type %1))))))
+
+(def sample-templates (merge-all-template {:all {:package "net.lfn3.sprue"}
+                                           :id  {:extends ["net.lfn3.sprue" "Id"]}}))
+
+(def sample-classes (->> [{:name   "Test"
+                           :type   :data
+                           :fields [{:name "name"
+                                     :type String}]}]
+                         (merge-templates sample-templates)
+                         (with-ids)
+                         (merge-templates sample-templates)))
+
+(defn ^FileSpec wrap-in-file [{:keys [package name] :as class-config}]
+  (.build (doto (FileSpec/builder package name)
+     (.addType (make-class class-config)))))
+
+(defn write-to-str [^FileSpec fs]
+  (let [sw (StringWriter.)]
+    (.writeTo fs sw)
+    (str sw)))
+
+(defn str-classes [classes]
+  (->> classes (map wrap-in-file) (map write-to-str) (apply str)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (print (str-class sample-config)))
+  (print (str-classes sample-classes)))
